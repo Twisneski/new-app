f9703aeacb9067f98c9a9df3c0348011439cc6e0
Twisneski <tina.wisneski525@gmail.com>
2016-03-26 00:32:50 -0500

    added server and jade files

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var express = require('express');
+var stormpath = require('express-stormpath');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true
+  }
+}));
+
+app.get('/', stormpath.getUser, (req, res) => {
+  res.render('home', {
+    title: 'Welcome'
+  });
+});
+
+app.get('/budget', (req, res) => {
+  res.render('budget', {
+    title:  'Budget'
+  });
+});
+
+app.get('/actual', (req, res) => {
+  res.render('actual', {
+    title:  'Actual Cam Amounts'
+  });
+});
+
+app.use('/newLease',stormpath.loginRequired,require('./newLease')());
+
+app.on('stormpath.ready',function(){
+  console.log('Stormpath Ready');
+});
+app.listen(3000);
+
+
